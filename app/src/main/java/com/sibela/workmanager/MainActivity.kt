3cae836cfea23c9060747a1a9849e7cb2264cb59
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-29 10:17:29 -0300

    Scheduling work

--- app/src/main/java/com/sibela/workmanager/MainActivity.kt
+++ app/src/main/java/com/sibela/workmanager/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.sibela.workmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.sibela.workmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            ToastScheduler.schedule(baseContext, "Leonardo")
+        }
     }
 }
